c1c2390afc51a5959deba5ffcee67416750535a0
Dukeyes <daniel.dik@hotmail.com>
2023-02-27 10:25:14 -0300

    .bat atualizado

--- front/AlgoriusFrontConversion.bat
+++ front/AlgoriusFrontConversion.bat
@@ -2,11 +2,11 @@
 :inicio
 title AlgoriusMake
 pyside6-uic login.ui -o ui_login.py
-echo ----------------------done with login1----------------------
+echo ----------------------done with login----------------------
 pyside6-uic form.ui -o ui_form.py
 echo ----------------------done with form----------------------
 pyside6-uic load.ui -o ui_load.py
-echo ----------------------done with load 1----------------------
+echo ----------------------done with load----------------------
 pyside6-uic register.ui -o ui_register.py
 echo ----------------------done with register----------------------
 pyside6-rcc .\rsc.qrc -o .\rsc_rc.py
